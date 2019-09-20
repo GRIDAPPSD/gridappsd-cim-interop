4e6ff325dfb754dae92f9ed7e08c7614c0c7acaf
Craig A <craig.allwardt@pnnl.gov>
2019-09-20 09:28:38 -0700

    Added more files for creating a client

--- derms_app/server.py
+++ derms_app/server.py
@@ -12,8 +12,23 @@ def root():
     return app.send_static_file('index.html')
 
 
-@app.route('/confirmation', methods = ['POST'])
+@app.route('/static/<path:path>')
+def send_static_html(path):
+    return send_from_directory('static', path)
+
+
+@app.route('/api/create')
+def create_group():
+    # Build an xml structure to send to openderms
+    # use zeep to send that xml structure to openderms/test instance and get response
+    pass
+
+
+@app.route('/confirmation', methods=['POST'])
 def printMesasge():
+    # Build an xml structure to send to openderms
+    # use zeep to send that xml structure to openderms/test instance and get response
+
     testName = request.form['textName']
     print('testName: ' + testName)
     message = request.form['message']
